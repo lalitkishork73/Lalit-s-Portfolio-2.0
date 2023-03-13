20e1c18caa0bd36e920a9c2b9688f1bf68dbe147
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2023-03-13 23:12:45 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div className='relative z-0 bg-primary'>
-        <div className=''>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
         </div>
